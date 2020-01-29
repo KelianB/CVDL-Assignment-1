c662ec2c4e0aa59da1f5f497c4fb906d7d71a99c
KelianB <kelian.baert@gmail.com>
2020-01-29 10:17:00 +0100

    refactoring and comments

--- assignment1/task2.py
+++ assignment1/task2.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from task2a import cross_entropy_loss, BinaryModel, pre_process_images
 np.random.seed(0)
 
-
 def calculate_accuracy(X: np.ndarray, targets: np.ndarray, model: BinaryModel) -> float: 
     """
     Args:
@@ -29,7 +28,7 @@ def train(
         num_epochs: int,
         learning_rate: float,
         batch_size: int,
-        l2_reg_lambda: float # Task 3 hyperparameter. Can be ignored before this.
+        l2_reg_lambda: float
         ):
     """
         Function that implements logistic regression through mini-batch
@@ -59,10 +58,9 @@ def train(
             # Forward pass
             train_outputs = model.forward(X_batch)
             
-            # Backward
+            # Backward propagation
             model.backward(X_batch, train_outputs, Y_batch)
             model.w -= learning_rate * model.grad
-
 
             # Track training loss continuously
             _train_loss = cross_entropy_loss(Y_batch, train_outputs)
@@ -81,6 +79,7 @@ def train(
 
             global_step += 1
 
+        # Compute validation loss for early stopping
         val_outputs = model.forward(X_val)
         _val_loss = cross_entropy_loss(Y_val, val_outputs)
         if _val_loss <= best_val_loss:
@@ -93,18 +92,9 @@ def train(
     return model, train_loss, val_loss, train_accuracy, val_accuracy
 
 
-# Load dataset
+# Load dataset (X_ = image data, Y_ = targets)
 category1, category2 = 2, 3
 validation_percentage = 0.1
-
-"""
-    X = images
-    Y = targets
-    _train suffix = training data
-    _val suffix = validation data
-    _test suffix = test data
-
-"""
 X_train, Y_train, X_val, Y_val, X_test, Y_test = utils.load_binary_dataset(
     category1, category2, validation_percentage)
 
@@ -112,17 +102,20 @@ X_train = pre_process_images(X_train)
 X_val = pre_process_images(X_val)
 X_test = pre_process_images(X_test)
 
-# hyperparameters
+# Hyperparameters
 num_epochs = 50
 learning_rate = 0.2
 batch_size = 128
 l2_reg_lambda = 0
+
+# Train
 model, train_loss, val_loss, train_accuracy, val_accuracy = train(
     num_epochs=num_epochs,
     learning_rate=learning_rate,
     batch_size=batch_size,
     l2_reg_lambda=l2_reg_lambda)
 
+# Print final loss
 print("Final Train Cross Entropy Loss:",
       cross_entropy_loss(Y_train, model.forward(X_train)))
 print("Final Validation Cross Entropy Loss:",
@@ -130,26 +123,25 @@ print("Final Validation Cross Entropy Loss:",
 print("Final Test Cross Entropy Loss:",
       cross_entropy_loss(Y_val, model.forward(X_val)))
 
-
+# Print final accuracy
 print("Train accuracy:", calculate_accuracy(X_train, Y_train, model))
 print("Validation accuracy:", calculate_accuracy(X_val, Y_val, model))
 print("Test accuracy:", calculate_accuracy(X_test, Y_test, model))
-
 
 # Plot loss
 plt.ylim([0., .4]) 
 utils.plot_loss(train_loss, "Training Loss")
 utils.plot_loss(val_loss, "Validation Loss")
+plt.xlabel("Gradient steps")
 plt.legend()
 plt.savefig("binary_train_loss.png")
 plt.show()
-
 
 # Plot accuracy
 plt.ylim([0.93, .99])
 utils.plot_loss(train_accuracy, "Training Accuracy")
 utils.plot_loss(val_accuracy, "Validation Accuracy")
-
+plt.xlabel("Gradient steps")
 plt.legend()
 plt.savefig("binary_train_accuracy.png")
 plt.show()
